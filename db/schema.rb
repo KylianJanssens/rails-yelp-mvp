fe77ff8d8c8303e1c299d7c81128ab7e064097ea
Kylian Janssens <kylianjanssens@hotmail.com>
2022-10-20 18:55:49 +0200

    added phone number tot database

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_18_190138) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_20_165110) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "phone_number"
   end
 
 end
