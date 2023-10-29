9e652a729d7f4f5c6618a2e41a5cba19bbcf0d9f
Kylian Janssens <kylianjanssens@hotmail.com>
2023-10-29 16:38:02 +0100

    Refactored

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,11 +9,10 @@ class ReviewsController < ApplicationController
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
-    @review.rating = params[:review][:rating].to_i 
+    @review.rating = params[:review][:rating].to_i
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      # render :new, status: :unprocessable_entity
       render 'restaurants/show'
     end
   end
